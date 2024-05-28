af66580665157adc752f2ad40e537c09e48cfbbf
jaiminiprajapati <jaiminihp397@gmail.com>
2024-05-28 09:04:10 +0100

    Routes created for pages

---
+++ src/pages/ProjectsPage/projects.js
@@ -0,0 +1,77 @@
+import cinemate from "../../assets/cinemate.png";
+import flowify from "../../assets/flowify.png";
+import digitalclock from "../../assets/digitalclock.png";
+import weatherdashboard from '../../assets/weatherdashboard.png'
+import codequiz from '../../assets/codequiz.png'
+
+const projects = [
+  {
+    previewLink: "https://jaiminiprajapati.github.io/movieland/",
+    githubLink: "https://github.com/jaiminiprajapati/movieland",
+    projectName: "Cine Mate",
+    projectDetails: "A responsive movie search web application named Cine Mate using React.js. The application allows users to search for movies using the Open Movie Database (OMDb) API.",
+    demoImage: cinemate,
+    skill_img: [
+      "https://cdn.worldvectorlogo.com/logos/react-2.svg",
+      "https://cdn.worldvectorlogo.com/logos/html-1.svg",
+      "https://cdn.worldvectorlogo.com/logos/css-3.svg",
+      "https://cdn.worldvectorlogo.com/logos/javascript-1.svg",
+    ],
+  },
+  {
+    previewLink: "https://flowify.netlify.app/",
+    githubLink: "https://github.com/jaiminiprajapati/schedule-organizer",
+    projectName: "Flowify",
+    projectDetails: "It is a schedule organizer application designed to simplify scheduling and event management. It offers a user-friendly interface for organizing schedules, managing tasks, and planning events with ease.",
+    demoImage: flowify,
+    key: "Flowify",
+    skill_img: [
+      "https://cdn.worldvectorlogo.com/logos/react-2.svg",
+      "https://cdn.worldvectorlogo.com/logos/html-1.svg",
+      "https://cdn.worldvectorlogo.com/logos/css-3.svg",
+      "https://cdn.worldvectorlogo.com/logos/javascript-1.svg",
+    ],
+  },
+  {
+    previewLink: "https://jaiminiprajapati.github.io/Digital-Clock/",
+    githubLink: "https://github.com/jaiminiprajapati/Digital-Clock",
+    projectName: "Digital Clock",
+    projectDetails: "It is a static webpage showing current time created using pure HTML, CSS and Javascript.",
+    demoImage: digitalclock,
+    key: "Digital Clock",
+    skill_img: [
+      "https://cdn.worldvectorlogo.com/logos/html-1.svg",
+      "https://cdn.worldvectorlogo.com/logos/css-3.svg",
+      "https://cdn.worldvectorlogo.com/logos/javascript-1.svg",
+    ],
+  },
+  {
+    previewLink: "https://jaiminiprajapati.github.io/Module08-WeatherDashboard/",
+    githubLink: "https://github.com/jaiminiprajapati/Module08-WeatherDashboard",
+    projectName: "Weather Dashboard",
+    projectDetails: "The Weather Dashboard is a web application that allows users to check the current weather conditions and a 5-day forecast for a specific city. Additionally, it keeps track of the user's search history.",
+    demoImage: weatherdashboard,
+    key: "Weather Dashboard",
+    skill_img: [
+      "https://cdn.worldvectorlogo.com/logos/html-1.svg",
+      "https://cdn.worldvectorlogo.com/logos/css-3.svg",
+      "https://cdn.worldvectorlogo.com/logos/javascript-1.svg",
+    ],
+  },
+  {
+    previewLink: "https://jaiminiprajapati.github.io/Module06-CodeQuiz/",
+    githubLink: "https://github.com/jaiminiprajapati/Module06-CodeQuiz",
+    projectName: "Code Quiz Application",
+    projectDetails: "Code Quiz is an engaging and interactive application designed to test and enhance your coding knowledge.",
+    demoImage: codequiz,
+    key: "Code Quiz Application",
+    skill_img: [
+      "https://cdn.worldvectorlogo.com/logos/html-1.svg",
+      "https://cdn.worldvectorlogo.com/logos/css-3.svg",
+      "https://cdn.worldvectorlogo.com/logos/javascript-1.svg",
+    ], 
+  },
+  // ...other projects
+];
+
+export default projects;
